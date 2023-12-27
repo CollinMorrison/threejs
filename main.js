7ae50d4e01e0eddd2317a540a67c71d46d594e60
Collin Morrison <morricollin@gmail.com>
2023-12-26 21:26:17 -0700

    Added edges and rotation

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// to run, run 'npx vite'
+
+
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
@@ -11,7 +14,11 @@ document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+const edges = new THREE.EdgesGeometry(geometry);
+const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0xffffff } ));
+scene.add(line);
 scene.add( cube );
+
 
 camera.position.z = 5;
 
@@ -19,6 +26,8 @@ function animate () {
     requestAnimationFrame( animate )
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    line.rotation.x += 0.01;
+    line.rotation.y += 0.01;
     renderer.render( scene, camera )
 }
 animate();
